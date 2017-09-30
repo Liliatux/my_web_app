589b679e32d3d59df5cd5061d97ff2b3360b6eb2
Lilia <geertszelia@gmail.com>
2017-09-30 14:28:51 +0200

    components: update footer with form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,24 @@
 import React, { Component } from 'react'
-import { Grid, Image, Header, List } from 'semantic-ui-react'
+import { Grid, Header } from 'semantic-ui-react'
 
-import FooterList from './FooterList'
+import Contact from './contact'
 
 class Footer extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      url: {
-        react: 'https://facebook.github.io/react/',
-        redux: 'http://redux.js.org/docs/introduction/',
-        semantic: 'https://react.semantic-ui.com/introduction'
-      },
-      name: {
-        react: 'react',
-        redux: 'redux',
-        semantic: 'semantic-ui-react'
-      }
-    }
+  handleSubmit = (value) => {
+    console.log(value)
   }
 
   render() {
     return (
-      <Grid columns={3} className='backgroundPurple colorWhite'>
+      <Grid className='backgroundPurple colorWhite'>
         <Grid.Row>
           <Grid.Column textAlign='center'>
-            <Image src='./image/react.png' className='react' size='tiny' spaced />
+            <Header as='h2' content='Contact' inverted />
           </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered columns={3}>
           <Grid.Column>
-            <Header as='h4' className='colorWhite'>Technologies Utilisées</Header>
-            <List>
-              <FooterList url={this.state.url.react} name={this.state.name.react} />
-              <FooterList url={this.state.url.redux} name={this.state.name.redux} />
-              <FooterList url={this.state.url.semantic} name={this.state.name.semantic} />
-            </List>
-          </Grid.Column>
-          <Grid.Column>
+            <Contact onSubmit={this.handleSubmit} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
