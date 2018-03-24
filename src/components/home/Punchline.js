78a750a21fd68cbdf55cb41219cd28bb01960e98
Zélia Geerts <geertszelia@gmail.com>
2018-03-24 18:55:37 +0100

    home: update punchline

--- src/components/home/Punchline.js
+++ src/components/home/Punchline.js
@@ -5,7 +5,7 @@ const Punchline = () => (
   <Grid.Row className='background marginRows'>
     <Header textAlign='center'>
       <Header size='huge' className='familyCode'><span className="surligne">Développeuse Web</span></Header>
-      <Header.Content className='familyCode'><span className='surligne'>"D'un naturel calme et ouverte d'esprit, j'aime faire de la veille technologique."</span></Header.Content>
+      <Header.Content className='familyCode'><span className='surligne'>"D'un naturel calme et ouverte d'esprit, j'aime constament me former."</span></Header.Content>
       <Header.Content className='familyCode'><span className='surligne'>"Passionnée et exigente dans mon travail, je ne refuse jamais un challenge."</span></Header.Content>
     </Header>
   </Grid.Row>
